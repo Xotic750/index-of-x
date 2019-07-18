d023d25087ab0457947714f453100f7a71bc13fd
Graham Fairweather <xotic750@gmail.com>
2019-07-19 00:28:49 +0200

    :bookmark: v3.0.7
    Auto build

--- dist/index-of-x.js
+++ dist/index-of-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-18T00:14:37.863Z",
+  "date": "2019-07-18T22:28:43.596Z",
   "describe": "",
   "description": "An extended ES6 indexOf.",
   "file": "index-of-x.js",
   "hash": "26d977f3e9d57c506590",
   "license": "MIT",
-  "version": "3.0.6"
+  "version": "3.0.7"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
