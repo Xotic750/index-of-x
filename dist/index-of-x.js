85b10daf76c68d73cca79f9a9e669da92030d7c5
Graham Fairweather <xotic750@gmail.com>
2019-07-22 15:04:12 +0200

    :bookmark: v3.0.10
    Dependency updates

--- dist/index-of-x.js
+++ dist/index-of-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-21T23:04:48.368Z",
+  "date": "2019-07-22T13:04:05.703Z",
   "describe": "",
   "description": "An extended ES6 indexOf.",
   "file": "index-of-x.js",
   "hash": "26d977f3e9d57c506590",
   "license": "MIT",
-  "version": "3.0.9"
+  "version": "3.0.10"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
