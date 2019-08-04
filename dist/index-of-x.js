9730673fd8e7bac4ec615cfc883f4adc454f17e1
Graham Fairweather <xotic750@gmail.com>
2019-08-04 17:21:59 +0200

    Auto build

--- dist/index-of-x.js
+++ dist/index-of-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-31T22:47:23.999Z",
+  "date": "2019-08-04T15:21:52.127Z",
   "describe": "",
   "description": "An extended ES6 indexOf.",
   "file": "index-of-x.js",
